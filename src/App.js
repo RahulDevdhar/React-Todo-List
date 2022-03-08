bd7d2a32a6ea0afd55b857c134939ff1cfeacf57
Rahul Devdhar <rahuldevdhar53139@gmail.com>
2022-03-08 14:24:01 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Header.js';
+import React from 'react'
+
+function App() {
+  return (
+      <>
+    <Header/>
+    </>
+  );
+}
+export default App;
---
+++ src/rahul.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function devang() {
+  return (
+    <div>rahul</div>
+  )
+}
+import React from 'react'
+
+export const rahul = () => {
+  return (
+    <div>rahul</div>
+  )
+}
+import React, { Component } from 'react'
+
+export default class rahul extends Component {
+  render() {
+    return (
+      <div>rahul</div>
+    )
+  }
+}
+
