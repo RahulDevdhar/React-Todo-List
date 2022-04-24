d5f0b532d817f78ccb399c50574d1d81cb26d79b
Rahul Devdhar <rahuldevdhar53139@gmail.com>
2022-04-24 14:34:41 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
-import Header from './Components/Header.js';
-import React from 'react'
+import Header from './Components/Header';
+import {About} from './Components/About';
+import {Footer} from './Components/Footer';
+import {Todo} from './Components/Todo';
 
-function App() {
+function App()
+
+  let todolist=[
+    {
+      sno:100,
+      title:"Learn React",
+      desc:"Learn React from scratch",
+    },
+    {
+      sno:200,
+      title:"Learn Java",
+      desc:"Learn Java from scratch",
+    },
+    {
+      sno:300,
+      title:"Learn Flutter",
+      desc:"Learn Flutter from scratch",
+    }
+  ]
+
+  const onDelete = () =>{
+    console.log("I am Delete");
+  }
+  
   return (
-      <>
-    <Header/>
+    <>
+      <Header title="Rahul Todos"/>
+      <About/>
+      <Todo todolist={todolist} onDelete={onDelete}/>
+      <Footer/>
     </>
   );
 }
+
 export default App;
